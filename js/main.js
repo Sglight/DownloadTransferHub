fe616c5ee4346a0f004db234aa74f719ce503e18
Sglight <448174863@qq.com>
2020-12-29 23:44:26 +0800

    优化 JS，新增在输入框内按下回车可直接提交表单

--- js/main.js
+++ js/main.js
@@ -1,47 +1,3 @@
-function setNowNavItem(item) {
-  // 清空 nav-now 状态
-  let test = document.getElementsByClassName("nav-item");
-  for (let elem of test) {
-    elem.className = "nav-item";
-  }
-  // 添加 nav-now
-  document.getElementById(item).className = "nav-item nav-now";
-}
-
-function goParse() {
-  setNowNavItem("parse");
-  document.getElementById("inputFile").removeAttribute("hidden");
-  document.getElementById("remarks").removeAttribute("hidden");
-  document.getElementById("uploadFile").setAttribute("hidden", "hidden");
-  document.getElementById("mode").setAttribute("value", "parse");
-  document.getElementById("gogogo").setAttribute("value", "解析");
-  document.getElementById("gogogo").setAttribute("onclick", "doParse()");
-  document.getElementById("secretKey").style.width = "35%";
-
-}
-
-function goUpload() {
-  setNowNavItem("upload");
-  document.getElementById("uploadFile").removeAttribute("hidden");
-  document.getElementById("remarks").removeAttribute("hidden");
-  document.getElementById("inputFile").setAttribute("hidden", "hidden");
-  document.getElementById("mode").setAttribute("value", "upload");
-  document.getElementById("gogogo").setAttribute("value", "上传");
-  document.getElementById("gogogo").setAttribute("onclick", "doUpload()");
-  document.getElementById("secretKey").style.width = "35%";
-}
-
-function goSearch() {
-  setNowNavItem("search");
-  document.getElementById("inputFile").setAttribute("hidden", "hidden");
-  document.getElementById("uploadFile").setAttribute("hidden", "hidden");
-  document.getElementById("remarks").setAttribute("hidden", "hidden");
-  document.getElementById("mode").setAttribute("value", "search");
-  document.getElementById("gogogo").setAttribute("value", "查询");
-  document.getElementById("gogogo").setAttribute("onclick", "doSearch()");
-  document.getElementById("secretKey").style.width = "80%";
-}
-
 function doParse() {
   alert("doParse()");
 }
@@ -52,4 +8,85 @@ function doUpload() {
 
 function doSearch() {
   alert("doSearch()");
+}
+
+function goParse() {
+  setNowNavItem("parse");
+  hideElement("inputFile", false);
+  hideElement("uploadFile",true);
+  hideElement("remarks", false);
+  setElementValue("mode", "parse");
+  setElementValue("gogogo", "解析");
+  document.getElementById("secretKey").style.width = "35%";
+
+}
+
+function goUpload() {
+  setNowNavItem("upload");
+  hideElement("inputFile",true);
+  hideElement("uploadFile", false);
+  hideElement("remarks", false);
+  setElementValue("mode", "upload");
+  setElementValue("gogogo", "上传");
+  document.getElementById("secretKey").style.width = "35%";
+}
+
+function goSearch() {
+  setNowNavItem("search");
+  hideElement("inputFile",true);
+  hideElement("uploadFile",true);
+  hideElement("remarks",true);
+  setElementValue("mode", "search");
+  setElementValue("gogogo", "查询");
+  document.getElementById("secretKey").style.width = "80%";
+}
+
+function doTrident() {
+  let mode = document.getElementById("mode").getAttribute("value");
+  switch (mode) {
+    case "parse":
+      doParse();
+      break;
+    case "upload":
+      doUpload();
+      break;
+    case "search":
+      doSearch()
+      break;
+  }
+}
+
+function setNowNavItem(item) {
+  // 清空 nav-now 状态
+  let test = document.getElementsByClassName("nav-item");
+  for (let elem of test) {
+    elem.className = "nav-item";
+  }
+  // 添加 nav-now
+  document.getElementById(item).className = "nav-item nav-now";
+}
+
+// 按下回车提交表单
+// 只监听 form 里的按键事件
+const form = document.getElementById('form');
+
+form.addEventListener("keydown", function (event) {
+  if (event.defaultPrevented) {
+    return; // 如果事件已经在进行中，则不做任何事。
+  }
+  if (event.key === "Enter") {
+    doTrident();
+  }
+}, true);
+
+function hideElement(id, hidden) {
+  if (hidden) {
+    document.getElementById(id).setAttribute("hidden", "hidden");
+  } else {
+    document.getElementById(id).removeAttribute("hidden");
+  }
+}
+
+function setElementValue(id, value) {
+  document.getElementById(id).setAttribute("value", value);
 }
