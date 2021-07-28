5cc24d073f4efb85a25773d8c510c69c9a534798
Sglight <448174863@qq.com>
2021-07-28 22:31:43 +0800

    upload error

--- js/node-server.js
+++ js/node-server.js
@@ -134,7 +134,7 @@ exp.post('/upload', async (request, response) => {
 
         let SQLQuery = `
             INSERT INTO "UserFiles"("FileName", "Hash", "SecretKey", "remarks", "originlink", "ip", "ua") 
-            VALUES('${alterFileName}', '${hash}', '${secretKey}', '${remarks}', '${inputFileLink}', '${ip}', '${ua}') 
+            VALUES('${fileName}', '${hash}', '${secretKey}', '${remarks}', '${inputFileLink}', '${ip}', '${ua}') 
             RETURNING "FID"
         `
         const client = new pg.Client(pgConfig)
